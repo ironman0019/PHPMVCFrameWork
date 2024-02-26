714ad87f55a7493eceb775115b40f571cc80a019
ironman0019 <emadesavi@gmail.com>
2024-02-27 02:23:40 +0330

    Fix bug in migrations.php & change Themes

--- migrations.php
+++ migrations.php
@@ -4,7 +4,7 @@
 use App\controllers\AuthController;
 use emadisavi\phpmvc\Application;
 use App\controllers\SiteController;
-
+use App\models\RegisterModal;
 
 require_once __DIR__.'/vendor/autoload.php';
 
@@ -12,6 +12,9 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $config = [
+
+    'userClass' => RegisterModal::class,
+
     'db' => [
 
         'dsn' => $_ENV['DB_DSN'],
